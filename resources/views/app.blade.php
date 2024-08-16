2fea790ee7fd8c20f730441ae74e2306eee5aba6
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-08-15 23:17:05 -0400

    update page title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name') }}</title>
+        <title inertia>{{ 'JHrecipes' }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
