85ed79e5b94d8bad3b0a5476be4d8718e9c0a81b
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-08-17 16:12:15 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,25 @@ use App\Http\Controllers\FavoriteRecipeController;
 use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::redirect('/', '/recipes')
     ->name('home');
 
 Route::middleware('auth')->group(function () {
-
     Route::resource('/recipes', RecipeController::class);
     Route::resource('/favorite-recipes', FavoriteRecipeController::class)
         ->only(['index', 'store', 'destroy']);
-
     Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
 });
+
+// New routes
+Route::get('/legal', function () {
+    return Inertia::render('LegalInformation');
+})->name('legal');
+
+Route::get('/contact', function () {
+    return Inertia::render('Contact');
+})->name('contact');
 
 require __DIR__.'/auth.php';
