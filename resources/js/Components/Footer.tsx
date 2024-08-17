e850d772287ec7afb13602c6d8b2bc339f58fc72
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-08-17 17:01:20 -0400

    Update Footer.tsx

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/react';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-gray-200 py-4 mt-8">
+        <footer className="bg-gray-100 py-2">
             <div className="container mx-auto px-4">
                 <div className="flex justify-between items-center font-sans text-gray-700">
                     <p style={{ fontSize: '14px' }}>&copy; {new Date().getFullYear()} JHrecipes.</p>
