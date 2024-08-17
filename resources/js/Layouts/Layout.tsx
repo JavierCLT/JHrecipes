f2dd46378250317aab58fe3df84c9675edf66411
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-08-17 16:10:16 -0400

    Create Layout.tsx

---
+++ resources/js/Layouts/Layout.tsx
@@ -0,0 +1,20 @@
+// resources/js/Layouts/Layout.tsx
+import React from 'react';
+import Footer from '../Components/Footer';
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <main className="flex-grow">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default Layout;
