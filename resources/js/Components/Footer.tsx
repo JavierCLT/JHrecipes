0a8cd68873798148456bc4ac66aeb2f47de4ef03
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-08-17 16:40:46 -0400

    Update Footer.tsx

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
         <footer className="bg-gray-200 py-4 mt-8">
             <div className="container mx-auto px-4">
                 <div className="flex justify-between items-center">
-                    <p>&copy; {new Date().getFullYear()} JHrecipes. All rights reserved.</p>
+                    <p>&copy; {new Date().getFullYear()} JHrecipes </p>
                     <div>
                         <Link href="/legal" className="text-blue-600 hover:text-blue-800 mr-4">
                             Legal
