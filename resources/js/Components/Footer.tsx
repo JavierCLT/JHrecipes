577f3861f51e759e9ddf9e218142a2d746b0fe6f
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-08-17 16:09:22 -0400

    Create Footer.tsx

---
+++ resources/js/Components/Footer.tsx
@@ -0,0 +1,25 @@
+// resources/js/Components/Footer.tsx
+import React from 'react';
+import { Link } from '@inertiajs/react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-gray-200 py-4 mt-8">
+            <div className="container mx-auto px-4">
+                <div className="flex justify-between items-center">
+                    <p>&copy; {new Date().getFullYear()} JHrecipes. All rights reserved.</p>
+                    <div>
+                        <Link href="/legal" className="text-blue-600 hover:text-blue-800 mr-4">
+                            Legal
+                        </Link>
+                        <Link href="/contact" className="text-blue-600 hover:text-blue-800">
+                            Contact
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
